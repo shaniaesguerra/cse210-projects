ae5683817512212b6dc7a5db211d817db2309e57
shaniaesguerra <shaniae47@gmail.com>
2026-01-15 15:14:14 -0700

    added functionality to GetRandomPrompt()

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,6 +3,11 @@ public class PromptGenerator
     public List<string> _prompts;
     public string GetRandomPrompt()
     {
-        return "";
+        Random rnd = new Random();
+        //generate a number from 0 to max length of prompt list
+        int randomNumber = rnd.Next(_prompts.Count());
+
+        //Return the random prompt
+        return _prompts[randomNumber];
     }
 }
