c1ee44e3b0abf948f07fcfde955cff627ea64599
shaniaesguerra <shaniae47@gmail.com>
2026-01-20 12:48:57 -0700

    created constructors

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,4 +1,25 @@
 public class Fraction
 {
-    
+    private int _top;
+    private int _bottom;
+
+    //Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
 }
