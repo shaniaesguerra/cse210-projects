b716bc31dc56e56a7039b378e142f71541034f0f
shaniaesguerra <shaniae47@gmail.com>
2026-02-05 15:26:42 -0700

    initial commit - reflecting activity set up

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,45 @@
+using System.Net.NetworkInformation;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+
+    public ReflectingActivity(string name, string description, int duration, List<string> prompts, List<string> questions) : base(name, description, duration)
+    {
+        foreach (string p in prompts)
+        {
+            _prompts.Add(p);
+        }
+
+        foreach (string q in questions)
+        {
+            _questions.Add(q);
+        }
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+    
+    public void DisplayQuestions()
+    {
+        
+    }
+}
