a1e1355c0aaa1ed96f48046003917dd99b90ce01
shaniaesguerra <shaniae47@gmail.com>
2026-02-05 15:06:25 -0700

    initial commit- breathing activity set up

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,12 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+
+    }
+    
+    public void Run()
+    {
+        
+    }
+}
