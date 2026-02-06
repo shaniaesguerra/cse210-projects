c07c30bea01f5521ae29ce32b9316aa3f8b13b4d
shaniaesguerra <shaniae47@gmail.com>
2026-02-05 20:05:39 -0700

    added animations

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -4,7 +4,7 @@ public class BreathingActivity : Activity
     {
         
     }
-    
+
     public void Run()
     {
         Console.Clear();
@@ -17,11 +17,73 @@ public class BreathingActivity : Activity
         while (DateTime.Now < endTime)
         {
             Console.WriteLine("");
-            Console.Write("Breathe in... ");
-            ShowCountDown(3);
-            Console.Write("\n and breathe out... ");
-            ShowCountDown(6);
+            Console.Write("Breathe in ");
+            ShowBreatheIn(3);
+            Console.Write("\n and breathe out ");
+            ShowBreatheOut(6);
             Console.WriteLine("");
+        }
+    }
+
+    private void ShowBreatheIn(int seconds)
+    {
+        // . . o o O O  ===> breathing animation
+        List<string> animationStr = new List<string>();
+        animationStr.Add(".");
+        animationStr.Add(" ");
+        animationStr.Add(".");
+        animationStr.Add(" ");
+        animationStr.Add("o");
+        animationStr.Add(" ");
+        animationStr.Add("o");
+        animationStr.Add(" ");
+        animationStr.Add("O");
+        animationStr.Add(" ");
+        animationStr.Add("O");
+        animationStr.Add(" ");
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+
+        while (DateTime.Now < endTime)
+        {
+            foreach (string s in animationStr)
+            {
+                Console.Write(s);
+                Thread.Sleep(300);
+            }
+
+        }
+    }
+
+    private void ShowBreatheOut(int seconds)
+    {
+        // . . o o O O  ===> breathing animation
+        List<string> animationStr = new List<string>();
+        animationStr.Add("O");
+        animationStr.Add(" ");
+        animationStr.Add("O");
+        animationStr.Add(" ");
+        animationStr.Add("o");
+        animationStr.Add(" ");
+        animationStr.Add("o");
+        animationStr.Add(" ");
+        animationStr.Add(".");
+        animationStr.Add(" ");
+        animationStr.Add(".");
+        animationStr.Add(" ");
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+
+        while (DateTime.Now < endTime)
+        {
+            foreach (string s in animationStr)
+            {
+                Console.Write(s);
+                Thread.Sleep(600);
+            }
+
         }
     }
 }
