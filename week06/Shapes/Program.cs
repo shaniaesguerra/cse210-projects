62e8c635012bcb861d2843803c26c34b0539db98
shaniaesguerra <shaniae47@gmail.com>
2026-02-09 20:19:27 -0700

    W06 - adding circle class

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -8,12 +8,18 @@ class Program
         string sqColor = square.GetColor();
         double sqArea = square.GetArea();
 
-        Console.WriteLine($"Sqauare: color = {sqColor}, area = {sqArea}");
+        Console.WriteLine($"Sqauare: color = {sqColor}, area = {sqArea.ToString("F2")}");
 
         Rectangle rectangle = new Rectangle("orange", 7, 5);
         string rectColor = rectangle.GetColor();
         double rectArea = rectangle.GetArea();
-        Console.WriteLine($"Rectangle: color = {rectColor}, area = {rectArea}");
+        Console.WriteLine($"Rectangle: color = {rectColor}, area = {rectArea.ToString("F2")}");
+
+        Circle circle = new Circle("pink", 3);
+        string cColor = circle.GetColor();
+        double cArea = circle.GetArea();
+        Console.WriteLine($"Circle: color = {cColor}, area = {cArea.ToString("F2")}");
+
 
     }
 }
